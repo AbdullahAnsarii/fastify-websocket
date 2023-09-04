8137bee39cc5f9d1957049e236ca933596a85c59
Abdullah Ansari <aa9272137@gmail.com>
2023-09-04 17:26:19 +0400

    basic socket implementation done

--- server.ts
+++ server.ts
@@ -1,21 +1,52 @@
-import Fastify, { FastifyReply, FastifyRequest } from "fastify";
+import { SocketStream } from "@fastify/websocket";
+import { FastifyError, FastifyReply, FastifyRequest } from "fastify";
 
-const fastify = Fastify({
-  logger: {
-    transport: {
-      target: "pino-pretty",
-    },
-  },
+const fastify = require('fastify')()
+
+fastify.register(require('@fastify/websocket'))
+
+fastify.get('/hello', (request: FastifyRequest, reply: FastifyReply) => {
+    reply.send({
+        message: 'Hello Fastify'
+    });
 });
 
-fastify.get('/ping', async (request:FastifyRequest, reply:FastifyReply) => {
-  return 'pong\n'
+fastify.register(async function (fastify: any) {
+    fastify.get('/*', { websocket: true }, (connection: SocketStream, req: FastifyRequest) => {
+        connection.socket.on('message', (message: string) => {
+            // message.toString() === 'hi from client'
+            connection.socket.send('hi from wildcard route')
+        })
+    })
+
+    fastify.get('/', { websocket: true }, (connection: SocketStream, req: FastifyRequest) => {
+        connection.socket.on('message', (message: Buffer) => {
+            // message.toString() === 'hi from client'
+            connection.socket.send(message.toString() + "coming from fastify")
+        })
+        // Client disconnect
+        connection.socket.on('close', () => {
+            console.log('Client disconnected');
+        });
+    })
+    fastify.get('/digits', { websocket: true }, (connection:SocketStream, req:FastifyRequest) => {
+        let timer = setInterval(() => {
+            connection.socket.send(randomDigit(1, 10).toString());
+        }, 1000);
+        connection.socket.on('close', () => {
+            clearInterval(timer);
+        });
+    });
+
 })
 
-fastify.listen({ port: 8080 }, (err, address) => {
-  if (err) {
-    console.error(err)
-    process.exit(1)
-  }
-  console.log(`Server listening at ${address}`)
+fastify.listen({ port: 8080 }, (err: FastifyError) => {
+    if (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
 })
+
+function randomDigit(min:number, max:number) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
